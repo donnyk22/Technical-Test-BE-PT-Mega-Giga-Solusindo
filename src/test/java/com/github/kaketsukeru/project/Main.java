58648496d907733820974920022058bc491f72ea
Donny Kurniawan <4dnnyk@gmail.com>
2025-10-30 21:01:49 +0700

    init commit

---
+++ src/test/java/com/github/kaketsukeru/project/Main.java
@@ -0,0 +1,13 @@
+package com.github.kaketsukeru.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Main {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

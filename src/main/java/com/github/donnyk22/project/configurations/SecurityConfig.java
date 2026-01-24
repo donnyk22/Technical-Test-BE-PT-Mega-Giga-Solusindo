1cf6d1de76f9c81b9e46a021efeb16a78d0f5ed7
Donny Kurniawan <4dnnyk@gmail.com>
2026-01-24 10:54:06 +0800

    add additional security config

--- src/main/java/com/github/donnyk22/project/configurations/SecurityConfig.java
+++ src/main/java/com/github/donnyk22/project/configurations/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.github.donnyk22.project.configurations;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +38,49 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        // Other filter chains configurations
+        http.headers(headers -> headers
+            // XSS (Cross-Site Scripting) Protection
+            .contentSecurityPolicy(csp ->
+                csp.policyDirectives("default-src 'self'")
+            )
+            // Clickjacking Protection (iFrame)
+            .frameOptions(frame -> frame.deny())
+            // MIME-Sniffing Protection
+            .contentTypeOptions(Customizer.withDefaults())
+            // Must Use HTTPS (1 year)
+            // .httpStrictTransportSecurity(hsts ->
+            //     hsts.includeSubDomains(true).maxAgeInSeconds(31536000)
+            // )
+            // Referrer Policy
+            .referrerPolicy(referrer ->
+                referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy
+                    .STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
+            )
+        );
 
         return http.build();
+    }
+
+    // CORS Configuration
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+            "https://donnyk22.com",
+            "http://localhost:8080"
+        ));
+        config.setAllowedMethods(List.of(
+            "GET", "POST", "PUT", "DELETE"
+        ));
+        config.setAllowedHeaders(List.of(
+            "Authorization", "Content-Type"
+        ));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }

44146e910de755f2aa3b449c2e28e65d7306a6fa
Donny Kurniawan <4dnnyk@gmail.com>
2026-02-16 01:11:02 +0800

    add worker to async process

--- src/main/java/com/github/donnyk22/project/Main.java
+++ src/main/java/com/github/donnyk22/project/Main.java
@@ -3,11 +3,9 @@ package com.github.donnyk22.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableCaching
-@EnableAsync
 public class Main {
 
 	public static void main(String[] args) {

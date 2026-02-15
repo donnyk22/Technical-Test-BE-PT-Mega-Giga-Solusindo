acf96220d8303cf45530a291ca72e27b715febd0
Donny Kurniawan <4dnnyk@gmail.com>
2026-02-16 01:12:26 +0800

    refactor annotation enablecaching

--- src/main/java/com/github/donnyk22/project/Main.java
+++ src/main/java/com/github/donnyk22/project/Main.java
@@ -2,10 +2,8 @@ package com.github.donnyk22.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class Main {
 
 	public static void main(String[] args) {

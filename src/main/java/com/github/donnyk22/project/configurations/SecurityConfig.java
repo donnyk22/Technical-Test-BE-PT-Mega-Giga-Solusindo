ccca06a1152d83f6258cd8afbd45a5ca5c2239b8
Donny Kurniawan <4dnnyk@gmail.com>
2026-02-16 00:28:30 +0800

    fix 403 response on async method

--- src/main/java/com/github/donnyk22/project/configurations/SecurityConfig.java
+++ src/main/java/com/github/donnyk22/project/configurations/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import jakarta.servlet.DispatcherType;
 import lombok.AllArgsConstructor;
 
 @Configuration
@@ -27,6 +28,7 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                .dispatcherTypeMatchers(DispatcherType.ASYNC).permitAll() // Allow async requests
                 .requestMatchers(
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
